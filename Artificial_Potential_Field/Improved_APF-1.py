fe218c3820e5d1861855bb4574e232a0f69a85d5
YunShuiXin <inmerufu@outlook.com>
2018-11-23 09:58:36 +0800

    add Improved APF

---
+++ Artificial_Potential_Field/Improved_APF-1.py
@@ -0,0 +1,113 @@
+"""
+人工势场寻路算法实现
+改进人工势场，解决不可达问题，仍存在局部最小点问题
+"""
+from Original_APF import APF, Vector2d
+import matplotlib.pyplot as plt
+import math
+from matplotlib.patches import Circle
+import random
+
+
+def check_vec_angle(v1: Vector2d, v2: Vector2d):
+    v1_v2 = v1.deltaX * v2.deltaX + v1.deltaY * v2.deltaY
+    angle = math.acos(v1_v2 / (v1.length * v2.length)) * 180 / math.pi
+    return angle
+
+
+class APF_Improved(APF):
+    def __init__(self, start: (), goal: (), obstacles: [], k_att: float, k_rep: float, rr: float,
+                 step_size: float, max_iters: int, goal_threshold: float, is_plot=False):
+        self.start = Vector2d(start[0], start[1])
+        self.current_pos = Vector2d(start[0], start[1])
+        self.goal = Vector2d(goal[0], goal[1])
+        self.obstacles = [Vector2d(OB[0], OB[1]) for OB in obstacles]
+        self.k_att = k_att
+        self.k_rep = k_rep
+        self.rr = rr  # 斥力作用范围
+        self.step_size = step_size
+        self.max_iters = max_iters
+        self.iters = 0
+        self.goal_threashold = goal_threshold
+        self.path = list()
+        self.is_path_plan_success = False
+        self.is_plot = is_plot
+        self.delta_t = 0.01
+
+    def repulsion(self):
+        """
+        斥力计算, 改进斥力函数, 解决不可达问题
+        :return: 斥力大小
+        """
+        rep = Vector2d(0, 0)  # 所有障碍物总斥力
+        for obstacle in self.obstacles:
+            # obstacle = Vector2d(0, 0)
+            obs_to_rob = self.current_pos - obstacle
+            rob_to_goal = self.goal - self.current_pos
+            if (obs_to_rob.length > self.rr):  # 超出障碍物斥力影响范围
+                pass
+            else:
+                rep_1 = Vector2d(obs_to_rob.direction[0], obs_to_rob.direction[1]) * self.k_rep * (
+                        1.0 / obs_to_rob.length - 1.0 / self.rr) / (obs_to_rob.length ** 2) * (rob_to_goal.length ** 2)
+                rep_2 = Vector2d(rob_to_goal.direction[0], rob_to_goal.direction[1]) * self.k_rep * ((1.0 / obs_to_rob.length - 1.0 / self.rr) ** 2) * rob_to_goal.length
+                rep +=(rep_1+rep_2)
+        return rep
+
+
+if __name__ == '__main__':
+    # 相关参数设置
+    k_att, k_rep = 1.0, 0.8
+    rr = 3
+    step_size, max_iters, goal_threashold = .2, 500, .2  # 步长0.5寻路1000次用时4.37s, 步长0.1寻路1000次用时21s
+    step_size_ = 2
+
+    # 设置、绘制起点终点
+    start, goal = (0, 0), (15, 15)
+    is_plot = True
+    if is_plot:
+        fig = plt.figure(figsize=(7, 7))
+        subplot = fig.add_subplot(111)
+        subplot.set_xlabel('X-distance: m')
+        subplot.set_ylabel('Y-distance: m')
+        subplot.plot(start[0], start[1], '*r')
+        subplot.plot(goal[0], goal[1], '*r')
+    # 障碍物设置及绘制
+    obs = [[1, 4], [2, 4], [3, 3], [6, 1], [6, 7], [10, 6], [11, 12], [14, 14]]
+    print('obstacles: {0}'.format(obs))
+    for i in range(0):
+        obs.append([random.uniform(2, goal[1] - 1), random.uniform(2, goal[1] - 1)])
+
+    if is_plot:
+        for OB in obs:
+            circle = Circle(xy=(OB[0], OB[1]), radius=rr, alpha=0.3)
+            subplot.add_patch(circle)
+            subplot.plot(OB[0], OB[1], 'xk')
+    # t1 = time.time()
+    # for i in range(1000):
+
+    # path plan
+    if is_plot:
+        apf = APF_Improved(start, goal, obs, k_att, k_rep, rr, step_size, max_iters, goal_threashold, is_plot)
+    else:
+        apf = APF_Improved(start, goal, obs, k_att, k_rep, rr, step_size, max_iters, goal_threashold, is_plot)
+    apf.path_plan()
+    if apf.is_path_plan_success:
+        path = apf.path
+        path_ = []
+        i = int(step_size_ / step_size)
+        while (i < len(path)):
+            path_.append(path[i])
+            i += int(step_size_ / step_size)
+
+        if path_[-1] != path[-1]:  # 添加最后一个点
+            path_.append(path[-1])
+        print('planed path points:{}'.format(path_))
+        print('path plan success')
+        if is_plot:
+            px, py = [K[0] for K in path_], [K[1] for K in path_]  # 路径点x坐标列表, y坐标列表
+            subplot.plot(px, py, '^k')
+            plt.show()
+    else:
+        print('path plan failed')
+    # t2 = time.time()
+    # print('寻路1000次所用时间:{}, 寻路1次所用时间:{}'.format(t2-t1, (t2-t1)/1000))
--- Artificial_Potential_Field/ReadMe.md
+++ Artificial_Potential_Field/ReadMe.md
@@ -1,6 +1,11 @@
 # 人工势场寻路算法
 
-## Original Artificial Potential Filed
+## Original Artificial Potential Field Method
 
-+ 最基本的人工势场方法，会出现局部最小值问题导致寻路失败
++ 最基本的人工势场方法，会出现局部最小值问题导致寻路失败，此外还存在不可达问题（如下图）
 + ![](./Original-APF-Path-Plan.png)
+
+## Improved Artificial Potential Field Method-1
+
++ 改进斥力函数，在斥力函数中也考虑机器人到目标点的距离影响，从而解决不可达问题
++ ![](./Improved-APF-Path-Plan.png)
