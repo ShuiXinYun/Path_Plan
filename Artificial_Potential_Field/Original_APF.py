aefb065e8abd335d2f650ba6abea19ad7aa93681
YunShuiXin <inmerufu@outlook.com>
2018-11-23 09:58:47 +0800

    Original APF

--- Artificial_Potential_Field/Original_APF.py
+++ Artificial_Potential_Field/Original_APF.py
@@ -1,10 +1,12 @@
 """
 人工势场寻路算法实现
-最基本的人工势场，会出现局部最小情况导致规划失败
+最基本的人工势场，存在目标点不可达及局部最小点问题
 """
 import math
 import random
 from matplotlib import pyplot as plt
+from matplotlib.patches import Circle
+import time
 
 
 class Vector2d():
@@ -25,18 +27,23 @@ class Vector2d():
             self.deltaX = deltaY[0] - deltaX[0]
             self.deltaY = deltaY[1] - deltaX[1]
             self.length = math.sqrt(self.deltaX ** 2 + self.deltaY ** 2) * 1.0
-            if self.length>0:
+            if self.length > 0:
                 self.direction = [self.deltaX / self.length, self.deltaY / self.length]
             else:
                 self.direction = None
         else:
             self.length = math.sqrt(self.deltaX ** 2 + self.deltaY ** 2) * 1.0
-            if self.length>0:
+            if self.length > 0:
                 self.direction = [self.deltaX / self.length, self.deltaY / self.length]
             else:
                 self.direction = None
 
     def __add__(self, other):
+        """
+        + 重载
+        :param other:
+        :return:
+        """
         vec = Vector2d(self.deltaX, self.deltaY)
         vec.deltaX += other.deltaX
         vec.deltaY += other.deltaY
@@ -69,8 +76,9 @@ class APF():
     """
     人工势场寻路
     """
-    def __init__(self, start: Vector2d, goal: Vector2d, obstacles: [], k_att: float, k_rep: float, rr: float,
-                 step_size: float, max_iters: int):
+
+    def __init__(self, start: (), goal: (), obstacles: [], k_att: float, k_rep: float, rr: float,
+                 step_size: float, max_iters: int, goal_threshold: float, is_plot=False):
         """
         :param start: 起点
         :param goal: 终点
@@ -80,42 +88,47 @@ class APF():
         :param rr: 斥力作用范围
         :param step_size: 步长
         :param max_iters: 最大迭代次数
+        :param goal_threshold: 离目标点小于此值即认为到达目标点
+        :param is_plot: 是否绘图
         """
-        self.start = start
-        self.current_pos = start
-        self.goal = goal
-        self.obstacles = obstacles
+        self.start = Vector2d(start[0], start[1])
+        self.current_pos = Vector2d(start[0], start[1])
+        self.goal = Vector2d(goal[0], goal[1])
+        self.obstacles = [Vector2d(OB[0], OB[1]) for OB in obstacles]
         self.k_att = k_att
         self.k_rep = k_rep
         self.rr = rr  # 斥力作用范围
         self.step_size = step_size
         self.max_iters = max_iters
         self.iters = 0
+        self.goal_threashold = goal_threshold
         self.path = list()
         self.is_path_plan_success = False
+        self.is_plot = is_plot
+        self.delta_t = 0.01
 
     def attractive(self):
         """
         引力计算
-        :return:
+        :return: 引力
         """
-        att = (self.goal - self.current_pos) * self.k_att
+        att = (self.goal - self.current_pos) * self.k_att  # 方向由机器人指向目标点
         return att
 
-    def repultion(self):
+    def repulsion(self):
         """
         斥力计算
-        :return:
+        :return: 斥力大小
         """
-        rep = Vector2d(0, 0)
+        rep = Vector2d(0, 0)  # 所有障碍物总斥力
         for obstacle in self.obstacles:
             # obstacle = Vector2d(0, 0)
             t_vec = self.current_pos - obstacle
-            if (t_vec.length > self.rr):
+            if (t_vec.length > self.rr):  # 超出障碍物斥力影响范围
                 pass
             else:
                 rep += Vector2d(t_vec.direction[0], t_vec.direction[1]) * self.k_rep * (
-                        1.0 / t_vec.length - 1.0 / self.rr) / (t_vec.length ** 2)
+                        1.0 / t_vec.length - 1.0 / self.rr) / (t_vec.length ** 2)  # 方向由障碍物指向机器人
         return rep
 
     def path_plan(self):
@@ -123,41 +136,72 @@ class APF():
         path plan
         :return:
         """
-        while (self.iters < self.max_iters and (self.current_pos-self.goal).length > self.step_size):
-            f_vec = self.attractive() + self.repultion()
-            # step_size_low, step_size_up = self.step_size*0.9, self.step_size*1.2
-            # step_size = random.uniform(step_size_low, step_size_up)
+        while (self.iters < self.max_iters and (self.current_pos - self.goal).length > self.goal_threashold):
+            f_vec = self.attractive() + self.repulsion()
             self.current_pos += Vector2d(f_vec.direction[0], f_vec.direction[1]) * self.step_size
             self.iters += 1
             self.path.append([self.current_pos.deltaX, self.current_pos.deltaY])
-            plt.plot(self.current_pos.deltaX, self.current_pos.deltaY, '.b')
-            plt.pause(0.01)
-        if (self.current_pos-self.goal).length <= self.step_size:
+            if self.is_plot:
+                plt.plot(self.current_pos.deltaX, self.current_pos.deltaY, '.b')
+                plt.pause(self.delta_t)
+        if (self.current_pos - self.goal).length <= self.goal_threashold:
             self.is_path_plan_success = True
 
 
 if __name__ == '__main__':
-    start, goal= Vector2d(0, 0), Vector2d(20,20)
-    plt.plot(0, 0, '*r')
-    plt.plot(20, 20, '*r')
-    obs_list = []
-    for i in range(30):
-        obs_list.append([random.uniform(2,19), random.uniform(2,19)])
-        # obs_list.append([random.randint(2,19), random.randint(2,19)])
-    obs=list()
-    for OB in obs_list:
-        plt.plot(OB[0], OB[1], 'xk')
-        obs.append(Vector2d(OB[0], OB[1]))
-    k_att, k_rep = 1.0, 10.0  # k_rep太大易出现拐角太大
-    rr = 6
-    step_size, max_iters = .2, 500
-    apf = APF(start, goal, obs, k_att, k_rep, rr, step_size, max_iters)
+    # 相关参数设置
+    k_att, k_rep = 1.0, 100.0
+    rr = 3
+    step_size, max_iters, goal_threashold = .2, 500, .2  # 步长0.5寻路1000次用时4.37s, 步长0.1寻路1000次用时21s
+    step_size_ = 2
+
+    # 设置、绘制起点终点
+    start, goal = (0, 0), (15, 15)
+    is_plot = True
+    if is_plot:
+        fig = plt.figure(figsize=(7, 7))
+        subplot = fig.add_subplot(111)
+        subplot.set_xlabel('X-distance: m')
+        subplot.set_ylabel('Y-distance: m')
+        subplot.plot(start[0], start[1], '*r')
+        subplot.plot(goal[0], goal[1], '*r')
+    # 障碍物设置及绘制
+    obs = [[1, 4], [2, 4], [3, 3], [6, 1], [6, 7], [10, 6], [11, 12], [14, 14]]
+    print('obstacles: {0}'.format(obs))
+    for i in range(0):
+        obs.append([random.uniform(2, goal[1] - 1), random.uniform(2, goal[1] - 1)])
+
+    if is_plot:
+        for OB in obs:
+            circle = Circle(xy=(OB[0], OB[1]), radius=rr, alpha=0.3)
+            subplot.add_patch(circle)
+            subplot.plot(OB[0], OB[1], 'xk')
+    # t1 = time.time()
+    # for i in range(1000):
+
+    # path plan
+    if is_plot:
+        apf = APF(start, goal, obs, k_att, k_rep, rr, step_size, max_iters, goal_threashold, is_plot)
+    else:
+        apf = APF(start, goal, obs, k_att, k_rep, rr, step_size, max_iters, goal_threashold, is_plot)
     apf.path_plan()
     if apf.is_path_plan_success:
-        # path = apf.path
-        # print(path)
+        path = apf.path
+        path_ = []
+        i = int(step_size_ / step_size)
+        while (i < len(path)):
+            path_.append(path[i])
+            i += int(step_size_ / step_size)
+
+        if path_[-1] != path[-1]:  # 添加最后一个点
+            path_.append(path[-1])
+        print('planed path points:{}'.format(path_))
         print('path plan success')
-        # px, py = [K[0] for K in path], [K[1] for K in path]  # 路径点x坐标列表, y坐标列表
-        plt.show()
+        if is_plot:
+            px, py = [K[0] for K in path_], [K[1] for K in path_]  # 路径点x坐标列表, y坐标列表
+            subplot.plot(px, py, '^k')
+            plt.show()
     else:
         print('path plan failed')
+    # t2 = time.time()
+    # print('寻路1000次所用时间:{}, 寻路1次所用时间:{}'.format(t2-t1, (t2-t1)/1000))
